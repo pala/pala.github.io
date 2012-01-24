8d99ee620d7e61d40299e15cebe8d58d28484bac
Tao Zhang <ztpala@gmail.com>
2012-01-24 00:25:27 -0500

    设置newpost脚本的时区为美东

--- newpost.rb
+++ newpost.rb
@@ -20,6 +20,17 @@ if ARGV.empty? or ARGV[0].downcase == "--help" or ARGV[0].downcase == "-h"
   exit (ARGV.empty? ? 1 : 0)
 end
 
+#from http://www.java2s.com/Code/Ruby/Time/Converttimetotimezone.htm
+class Time
+  def convert_zone(to_zone)
+    original_zone = ENV["TZ"]
+    utc_time = dup.gmtime
+    ENV["TZ"] = to_zone
+    to_zone_time = utc_time.localtime
+    ENV["TZ"] = original_zone
+    return to_zone_time
+  end
+end
 class String
   
   # from ruby on rails (https://github.com/rails/rails)
@@ -45,14 +56,16 @@ TEMPLATE = "template.markdown"
 POSTS_DIR = "_posts"
 
 # Get the title and use it to derive the new filename
+t = Time.now
+t.convert_zone("US/Eastern")
 title = ARGV.join(" ")
-filename = "#{Time.now.strftime('%Y-%m-%d')}-#{title.parameterize}.markdown" 
+filename = "#{t.strftime('%Y-%m-%d')}-#{title.parameterize}.markdown" 
 filepath = File.join(POSTS_DIR, filename)
 
 # Load in the template and set the title
 post_text = File.read(TEMPLATE)
 post_text.gsub!('%%TITLE%%', title)
-post_text.gsub!('%%DATE%%', "#{Time.now.strftime('%Y-%m-%d')}")
+post_text.gsub!('%%DATE%%', "#{t.strftime('%Y-%m-%d')}")
 
 # Write out the post
 post_file = File.open(filepath, 'w')
