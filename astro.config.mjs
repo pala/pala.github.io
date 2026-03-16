cdf53795aeeb29a4bed204c45b8925ee2113a577
Tao Zhang <ztpala@gmail.com>
2026-03-15 20:17:28 -0400

    Fix 404 images in Markdown posts by forcing global passthrough image service

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'http://ztpala.com',
-  output: 'static'
+  output: 'static',
+  image: {
+    service: passthroughImageService()
+  }
 });
